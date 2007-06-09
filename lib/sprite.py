f4ff9dde5551ed3a1c8150adca0efcbb112766e2
timonator <timonator@8e1eaa87-e730-0410-9c0e-ffa806ca6fad>
2007-06-09 23:51:46 +0000

    oh my god this is so buggy! (also merged rageons new level)

--- lib/sprite.py
+++ lib/sprite.py
@@ -2,6 +2,7 @@ import res
 from OpenGL.GL import *
 import level
 from timing import timer
+import math
 
 class Sprite:
   def __init__(self, imagename):
@@ -48,28 +49,13 @@ class Sprite:
     glEnd()
     glEnable(GL_TEXTURE_2D)
 
-  def checkVerLine(self, x, y):
-    tilx = int(x)
-    tily = int(y)
-    endy = int(y + self.h)
-    
-    #glColor4f(1.0, 0.0, 0.0, 0.25)
-
-    #self.mark(x, y, 0, self.h)
-    if tily != y - (1.0 - self.h):
-      endy += 1
-
-    for ty in range(tily, endy) or [tily]:
-      #self.markTile(tilx, ty)
-      if ty >= self.lev.h or tilx >= self.lev.w:
-        return True
-      if self.lev.tileset.collision[self.lev.level[ty][tilx]] == 1:
-        return True
-
-    return False
-
   def pointCollides(self, x, y, vx, vy):
-    col = self.tileset.collision[self.level[y][x]]
+    if not 0 < x < self.lev.w:
+      return True
+    if not 0 < y < self.lev.h:
+      return True
+
+    col = self.lev.tileset.collision[self.lev.level[int(y)][int(x)]]
     rx = x % 1
     ry = y % 1
     if col == 0:   # air
@@ -143,70 +129,34 @@ class Sprite:
     else:
       return True
 
-  def checkHorLine(self, x, y):
-    tilx = int(x)
-    tily = int(y)
-    endx = int(x + self.w)
-    #glColor4f(0.0, 1.0, 0.0, 0.25)
-    #self.mark(x, y, self.w, 0)
-
-    if tilx != x - (1.0 - self.w):
-      endx += 1
-
-    for tx in range(tilx, endx) or [tilx]:
-      #self.markTile(tx, tily)
-      if tily >= self.lev.h or tx >= self.lev.w:
-        return True
-      if self.lev.tileset.collision[self.lev.level[tily][tx]] == 1:
-        return True
-      elif self.lev.tileset.collision[self.lev.level[tily][tx]] in [8, 9] and self.vy > 0 and tily - self.y >= self.h:
-        return True
-
-    return False
-
   def checkCollision(self, vx, vy):
-    hit = [False] * 4 # left, right, top, bottom
-    if vx > 0:
-      if self.checkVerLine(self.x + vx + self.w, self.y):
-        self.x = int(self.x + vx) - self.w + 1
-        #self.vx = 0
-        hit[1] = True
-      else:
-        self.x += vx
-    elif vx < 0:
-      if self.checkVerLine(self.x + vx, self.y):
-        self.x = int(self.x + vx) + 1
-        #self.vx = 0
-        hit[0] = True
-      else:
-        self.x += vx
+    if vx == 0 and vy == 0:
+      return False
+    hit = False
 
-    if vy > 0:
-      if self.checkHorLine(self.x, self.y + vy + self.h):
-        self.y = int(self.y + vy) - self.h + 1
-        self.vy = 0
-        self.physics = 'standing'
-        hit[3] = True
-      else:
-        self.y += vy
-        self.physics = 'falling'
-    elif vy < 0:
-      if self.checkHorLine(self.x, self.y + vy):
-        self.y = int(self.y + vy) + 1
-        self.vy = self.vy * -0.25
-        self.physics = 'falling'
-        hit[2] = True
-      else:
-        self.y += vy
-        self.physics = 'falling'
+    pxy = [self.x + vx, self.y + vy]
 
-    if hit[0]:
+    l = math.sqrt(vx ** 2 + vy ** 2)
+    pxy1 = [x / l for x in (vx, vy)]
+
+    while self.pointCollides(*(pxy + [vx, vy])) and (pxy[0] < 0) == (vx < 0) and (pxy[1] < 0) == (vy < 0):
+      pxy = [pxy[0] - pxy1[0] * 0.1, pxy[1] - pxy1[1] * 0.1]
+      hit = True
+
+    self.x = pxy[0]
+    self.y = pxy[1]
+
+    if hit and vx < 0:
+      self.vx = 0
       self.hitLeft()
-    elif hit[1]:
+    elif hit and vx > 0:
+      self.vx = 0
       self.hitRight()
-    if hit[2]:
+    if hit and vy < 0:
+      self.vy = 0
       self.hitTop()
-    elif hit[3]:
+    elif hit and vy > 0:
+      self.vy = 0
       self.hitBottom()
 
   def hitLeft(self):   pass
